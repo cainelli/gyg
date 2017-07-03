d480c3b57c6830172c2e029a93753ea217618214
Fernando Cainelli <fernando.cainelli@gmail.com>
2017-07-03 18:47:25 -0300

    added mysql dump import function and google docstring standard to functions.

--- helpers/setup-replica.py
+++ helpers/setup-replica.py
@@ -1,9 +1,44 @@
 #!/usr/bin/python
 import argparse
 import MySQLdb as mdb
+from subprocess import Popen, PIPE
+
+def mysql_dump(master_host, master_user, master_password, slave_host, slave_user, slave_password, database):
+  """ Running this function to create dump from master and import to slave.
+
+  Args:
+    master_host (str): Master hostname or IP address.
+    master_user (str): Master's username with root privileges.
+    master_pass (str): username's password.
+    slave_host (str): Slave Hostname or IP address to configure the replication.
+    slave_user (str): Slave's username with root privileges.
+    slave_password (str): username's password.
+
+  TODO:
+    - This function uses mysqldump/mysql CLI to dump the database from master to slave.
+    Use MySQLdb API should be better. 
+  """
+  con = mdb.connect(master_host, master_user, master_password)
+  cur = con.cursor(mdb.cursors.DictCursor)
+  
+  # locking tables.
+  cur.execute('FLUSH TABLES WITH READ LOCK')
+  
+  print('importing database[%s] dump from master. This can take a while.' % database)
+  cmd = "mysql -h %s -u %s --password=%s %s" % (slave_host, slave_user, slave_password, database)
+  mysql = Popen(cmd.split(), stdin=PIPE, stdout=PIPE)
+
+  cmd = "mysqldump -h %s -u %s --password=%s %s" % (master_host, master_user, master_password, database)
+  mysqldump = Popen(cmd.split(), stdout=mysql.stdin)
+  
+  mysql_stdout = mysql.communicate()[0]
+  mysqldump.wait()
+
+  # unlocking tables.
+  cur.execute('UNLOCK TABLES')
 
 def get_mysql_bin_seq(master_host, master_user, master_password):
-  ''' This function returns the current file name and position from master.:  
+  """ This function returns the current file name and position from master.:  
     SHOW MASTER STATUS;
     +------------------+----------+--------------+------------------+-------------------+
     | File             | Position | Binlog_Do_DB | Binlog_Ignore_DB | Executed_Gtid_Set |
@@ -11,16 +46,20 @@ def get_mysql_bin_seq(master_host, master_user, master_password):
     | mysql-bin.000024 | 10715811 |              |                  |                   |
     +------------------+----------+--------------+------------------+-------------------+
 
-  :param: master_host: Master hostname or IP address.
-  :param: master_user: Master's username with root privileges.
-  :param: master_pass: username's password.
+  Args:
+    master_host (str): Master hostname or IP address.
+    master_user (str): Master's username with root privileges.
+    master_pass (str): username's password.
  
-  :returns:arr: [file, position]
-  '''
+  Returns:
+    Array with file(str) and its position(int):
+
+      [file, position]
+  """
+  print('getting information from master...')
   con = mdb.connect(master_host, master_user, master_password)
   cur = con.cursor(mdb.cursors.DictCursor)
   
-  cur.execute('FLUSH TABLES WITH READ LOCK')
   cur.execute('SHOW MASTER STATUS')
   r = cur.fetchone()
   
@@ -28,18 +67,18 @@ def get_mysql_bin_seq(master_host, master_user, master_password):
   return  [r['File'], int(r['Position'])]
 
 def set_replication(slave_host, slave_user, slave_password, master_host, rep_user, rep_password, master_log_file, master_log_pos):
-  ''' This function setup the replication on the slave server.
-  
-  :param: slave_host: Slave Hostname or IP address to configure the replication.
-  :param: slave_user: Slave's username with root privileges.
-  :param: slave_password: username's password.
-  :param: master_host: Master host which you want to change(CHANGE MASTER TO MASTER_HOST=....).
-  :param: rep_user: Replication user with the proper role.
-  :param: rep_password: replication user's password.
-  :param: master_log_file: Current file from master. You can get this running `SHOW MASTER STATUS` on master or using get_mysql_bin_seq function.
-  :param: master_log_pos: Current log position from master. You can get this running `SHOW MASTER STATUS` on master or using get_mysql_bin_seq function.
-  '''
-
+  """ This function setup the replication on the slave server.
+  Args:
+    slave_host (str): Slave Hostname or IP address to configure the replication.
+    slave_user (str): Slave's username with root privileges.
+    slave_password (str): username's password.
+    master_host (str): Master host which you want to change(CHANGE MASTER TO MASTER_HOST=....).
+    rep_user (str): Replication user with the proper role.
+    rep_password (str): replication user's password.
+    master_log_file (str): Current file from master. You can get this running `SHOW MASTER STATUS` on master or using get_mysql_bin_seq function.
+    master_log_pos (int): Current log position from master. You can get this running `SHOW MASTER STATUS` on master or using get_mysql_bin_seq function.
+  """
+  print('configuring replication...')
   con = mdb.connect(slave_host, slave_user, slave_password)
   cur = con.cursor(mdb.cursors.DictCursor)
     
@@ -83,6 +122,10 @@ if __name__ == '__main__':
                       required=True,
                       help='Replication password.')
 
+  parser.add_argument('--database', dest='database', action='store',
+                      required=False,
+                      help='Specify the database which the data will be copied from.')
+
   args = parser.parse_args()
   
   try:
@@ -91,6 +134,14 @@ if __name__ == '__main__':
                                                         master_user=args.master_user,
                                                         master_password=args.master_password)
 
+    # import database from master.
+    mysql_dump(master_host=args.master_host,
+                master_user=args.master_user,
+                master_password=args.master_password,
+                slave_host=args.slave_host,
+                slave_user=args.slave_user,
+                slave_password=args.slave_password,
+                database=args.database)
 
     # configure the replication on the slave side.
     set_replication(slave_host=args.slave_host, 
